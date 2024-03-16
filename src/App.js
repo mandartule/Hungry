dd3a62b514c1e4f8cf171e4140619cbb5e8a2a81
Mandar Tule <mandartule5@gmail.com>
2024-03-16 11:56:16 +0530

    routing and about us page added

--- src/App.js
+++ src/App.js
@@ -99,7 +99,8 @@ import ReactDOM from "react-dom/client"
 import Header from "./components/Header"
 import Body from "./components/Body"
 
-
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import About from "./components/About";
 
 
 const AppLayout = () => (
@@ -110,6 +111,18 @@ const AppLayout = () => (
     </div>
 
 )
+
+const appRouter = createBrowserRouter([
+    {
+        path: "/",
+        element: <AppLayout />
+    },
+    {
+        path: "/about",
+        element: <About />
+    
+    }
+]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<AppLayout />);
+root.render(<RouterProvider router = {appRouter}/>);
---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+
+const About = () => {
+
+    return (
+        <div>
+            <h1>About</h1>
+            <p>This is the about page of Namasate REACT 🚀</p>
+        </div>
+    )
+};
+
+export default About;
